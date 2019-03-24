d6a96fa143344f2783d852cc1b3b1e4f7d82a03e
dinkelk <kevin.dinkel@lasp.colorado.edu>
2019-03-23 23:32:06 -0600

    added monte carlo analysis with histogram plot

--- stocker.py
+++ stocker.py
@@ -1,5 +1,13 @@
 import numpy.random
 import copy
+import statistics
+import astropy.stats
+
+def format_currency(value):
+  return "${:,.2f}".format(value)
+
+def format_percentage(decimal):
+  return ("%0.1f%% " % (decimal*100.0))
 
 class Position(object):
   # ave return and std are in percentage per time unit:
@@ -25,8 +33,7 @@ class Position(object):
       self.value = 0.0
 
   def __repr__(self):
-    #return "$%0.2f" % self.value
-    return "${:,.2f}".format(self.value)
+    return format_currency(self.value)
 
 class Portfolio(object):
   def __init__(self, name, value, positions=[], weights=[]):
@@ -70,15 +77,13 @@ class Portfolio(object):
   def __repr__(self):
     value = self.value()
     template = "{0:<30}|{1:>12}|{2:>15}"
-    #strn += "\tPosition:\tAllocation:\tValue:\n"
     strn = "-----------------------------------------------------------\n"
     strn += template.format("Position", "Allocation ", "Value") + "\n"
     strn += "-----------------------------------------------------------\n"
     for w, p in zip(self.weights, self.positions):
-      #strn += "\t" + p.name + "\t" + ("%0.2f" % (w*100.0)) + "%\t" + str(p) + "\n"
-      strn += template.format(p.name, ("%0.1f%% " % (p.value/value*100.0)), str(p)) + "\n"
+      strn += template.format(p.name, format_percentage(p.value/value), str(p)) + "\n"
     strn += "-----------------------------------------------------------\n"
-    strn += template.format("Total", "100.0% ", "${:,.2f}".format(value)) + "\n"
+    strn += template.format("Total", "100.0% ", format_currency(value)) + "\n"
     strn += "-----------------------------------------------------------\n"
     return strn
 
@@ -90,14 +95,27 @@ class Assumptions(object):
     pass
 
 class Scenario(object):
-  def __init__(self, name, portfolio):
+  def __init__(self, name, portfolio, num_years, addition_per_year=0.0, addition_increase_perc=0.0, inflation_rate_perc=3.5, rebalance=True):
     self.name = name
     self.portfolio = portfolio
+    self.num_years = num_years
+    self.addition = addition_per_year
+    self.addition_increase = addition_increase_perc/100.0
+    self.inflation_rate = inflation_rate_perc/100.0
+    self.rebalance = rebalance
     self.history = [copy.deepcopy(portfolio)]
+    self.uncorrected_history = [copy.deepcopy(portfolio)]
     self.returns = []
-    self.inflation = []
+    self.uncorrected_returns = []
 
-  def simulate(self, addition=0.0, rebalance=True):
+  def reset(self):
+    self.portfolio = copy.deepcopy(self.history[0])
+    self.history = [copy.deepcopy(self.portfolio)]
+    self.uncorrected_history = [copy.deepcopy(self.portfolio)]
+    self.returns = []
+    self.uncorrected_returns = []
+
+  def _simulate(self, year, addition=0.0):
     # Buy more of the portfolio, after simulation, worst case:
     self.portfolio.trade(addition)
 
@@ -105,52 +123,124 @@ class Scenario(object):
     self.portfolio.simulate()
 
     # Rebalance portfolio to match original allocation weights:
-    if rebalance:
+    if self.rebalance:
       self.portfolio.rebalance()
 
-    # Save the porfolio in the history:
-    self.history.append(copy.deepcopy(self.portfolio))
+    # Correct the values in the portfolio for inflation to report
+    # numbers in today's dollars:
+    # Using the present value function to calculate what our money is actually worth with
+    # inflation: http://financeformulas.net/present_value.html
+    uncorrected_value = self.portfolio.value()
+    corrected_value = uncorrected_value*(1/(1 + self.inflation_rate)**year)
+    correction = corrected_value - uncorrected_value
+    p = copy.deepcopy(self.portfolio)
+    p.trade(correction)
+
+    # Save the corrected portfolio history:
+    self.history.append(p)
     return_perc = (self.history[-1].value() - self.history[-2].value())/self.history[-2].value()
     self.returns.append(return_perc)
 
-  def simulate_for(self, num_years, addition=0.0, addition_increase_perc=0.0, rebalance=True):
-    for x in range(num_years):
-      to_add = addition + addition_increase_perc*addition_increase_perc*x
-      self.simulate(to_add, rebalance)
+    # Save the uncorrected porfolio in the history:
+    self.uncorrected_history.append(copy.deepcopy(self.portfolio))
+    return_perc = (self.uncorrected_history[-1].value() - self.uncorrected_history[-2].value())/self.uncorrected_history[-2].value()
+    self.uncorrected_returns.append(return_perc)
 
-  # Calculate portfolio history in terms of today's dollars.
-  #def adjust_for_inflation(self):
-  #  # Calculate inflation rate:
-  #  inflation = inflation_rate/100 + numpy.random.normal(0, inflation_std_dev/100)
-  #  self.inflation.append(inflation)
-
-  #  # Correct for inflation:
-  #  # Using the present value function to calculate what our money is actually worth with
-  #  # inflation: http://financeformulas.net/present_value.html
-  #  value = self.portfolio.value()
-  #  corrected_value = value/(1 + inflation)
-  #  difference = corrected_value - value
-  #  self.portfolio.trade(difference)
+  def simulate(self):
+    to_add = self.addition
+    for x in range(self.num_years):
+      self._simulate(x+1, to_add)
+      to_add += to_add*self.addition_increase
 
   def __repr__(self):
     strn = self.name + " Scenario:\n"
     strn += "Portfolio: " + self.portfolio.name + " Portfolio\n"
     strn += "Duration: " + str(len(self.history)-1) + " years\n"
+    strn += "Annual Addition: " + format_currency(self.addition) + "\n"
+    strn += "Annual Addition Increase: " + format_percentage(self.addition_increase) + "\n"
+    strn += "Inflation Rate: " + format_percentage(self.inflation_rate) + "\n" 
+    strn += "Annual Rebalancing: " + ("Yes" if self.rebalance else "No") + "\n" 
     strn += "\n"
     strn += self.portfolio.name + " Portfolio Start:\n"
     strn += str(self.history[0])
     strn += "\n"
     strn += self.portfolio.name + " Portfolio End:\n"
+    strn += str(self.uncorrected_history[-1])
+    strn += "\n"
+    strn += self.portfolio.name + " Portfolio End (Inflation Corrected at " + format_percentage(self.inflation_rate) + "):\n" 
     strn += str(self.history[-1])
     strn += "\n"
-    strn += "Total Return: " + ("%0.1f%%" % (((self.history[-1].value() - self.history[0].value())/self.history[0].value())*100)) + "\n"
-    strn += "Ave Return: " + ("%0.1f%%" % (sum(self.returns)/len(self.returns)*100)) + "\n"
-    strn += "Best Return: " + ("%0.1f%%" % (max(self.returns)*100)) + " (year " + str(self.returns.index(max(self.returns))) + ")\n"
-    strn += "Worst Return: " + ("%0.1f%%" % (min(self.returns)*100)) + " (year " + str(self.returns.index(min(self.returns))) + ")\n"
+    strn += "Raw Metrics:\n"
+    strn += "Total Return: " + format_percentage((self.uncorrected_history[-1].value() - self.uncorrected_history[0].value())/self.uncorrected_history[0].value()) + "\n"
+    strn += "Ave Return: " + format_percentage(sum(self.uncorrected_returns)/len(self.uncorrected_returns)) + "\n"
+    strn += "Best Return: " + format_percentage(max(self.uncorrected_returns)) + " (year " + str(self.returns.index(max(self.returns))) + ")\n"
+    strn += "Worst Return: " + format_percentage(min(self.uncorrected_returns)) + " (year " + str(self.returns.index(min(self.returns))) + ")\n"
+    strn += "\n"
+    strn += "Inflation Corrected Metrics at " + format_percentage(self.inflation_rate) + ":\n" 
+    strn += "Total Return: " + format_percentage((self.history[-1].value() - self.history[0].value())/self.history[0].value()) + "\n"
+    strn += "Ave Return: " + format_percentage(sum(self.returns)/len(self.returns)) + "\n"
+    strn += "Best Return: " + format_percentage(max(self.returns)) + " (year " + str(self.returns.index(max(self.returns))) + ")\n"
+    strn += "Worst Return: " + format_percentage(min(self.returns)) + " (year " + str(self.returns.index(min(self.returns))) + ")\n"
     return strn
 
   def __str__(self):
     return self.__repr__()
+
+class Monte_Carlo(object):
+  def __init__(self, scenario):
+    self.scenario = copy.deepcopy(scenario)
+    self.scenario.reset()
+    self.runs = []
+    self.values = []
+    
+  def run(self, n):
+    for x in range(n):
+      new_scenario = copy.deepcopy(self.scenario)
+      new_scenario.simulate()
+      self.values.append(new_scenario.history[-1].value())
+      self.runs.append(new_scenario)
+
+  def __repr__(self):
+    strn = "Monte Carlo Results for " + str(len(self.runs)) + " Scenarios:\n"
+    strn += "Portfolio: " + self.scenario.portfolio.name + " Portfolio\n"
+    strn += "\n"
+    strn += "Inflation Corrected Portfolio Final Values:\n"
+    strn += "  Average:   " + format_currency(statistics.mean(self.values)) + "\n"
+    strn += "  Std Dev:   " + format_currency(statistics.stdev(self.values)) + "\n"
+    strn += "\n"
+    strn += "  Median:    " + format_currency(statistics.median_low(self.values)) + "\n"
+    strn += "  MAD:       " + format_currency(astropy.stats.median_absolute_deviation(self.values)) + "\n"
+    strn += "\n"
+    strn += "  Minimum:   " + format_currency(min(self.values)) + "\n"
+    strn += "  10th Perc: " + format_currency(numpy.percentile(self.values, 10)) + "\n"
+    strn += "  Median:    " + format_currency(statistics.median_low(self.values)) + "\n"
+    strn += "  90th Perc: " + format_currency(numpy.percentile(self.values, 90)) + "\n"
+    strn += "  Maximum:   " + format_currency(max(self.values)) + "\n"
+    return strn
+
+  def __str__(self):
+    return self.__repr__()
+
+  def histogram(self):
+    import matplotlib.pyplot as plt
+    values = [v/1000000.0 for v in self.values]
+    weights = numpy.ones_like(values)/float(len(values))*100.0
+    n, bins, patches = plt.hist(values, 30, weights=weights, facecolor='0.5', alpha=0.75)
+    plt.axvline(x=statistics.median_low(values), color='g')
+    plt.axvline(x=numpy.percentile(values, 10), color='r')
+    plt.axvline(x=statistics.median_low(values) - 2*astropy.stats.median_absolute_deviation(values), color='m')
+    plt.legend([ \
+      'Median (' + format_currency(statistics.median_low(self.values)) + ')', \
+      '10th Perc (' + format_currency(numpy.percentile(self.values, 10)) + ')', \
+      r'-2*MAD (' + format_currency(statistics.median_low(self.values) - 2*astropy.stats.median_absolute_deviation(self.values)) + ')', \
+    ])
+    plt.xlabel('Portfolio Value ($M)')
+    plt.ylabel('Probability %')
+    plt.title('Final Portfolio Value Probability Distribution (n=' + str(len(self.runs)) + ")")
+    #plt.text(60, .025, r'$\mu=100,\ \sigma=15$')
+    #plt.axis([40, 160, 0, 0.03])
+    plt.grid(True)
+    plt.show()
 
 if __name__== "__main__":
   us_stocks = Position("Domestic Equities", ave_return=10.2, std_dev=19.8)
@@ -162,8 +252,12 @@ if __name__== "__main__":
 
   sample_portfolio = Portfolio(name="Sample", value=100000.0, positions=[us_stocks, int_stocks, us_bonds, int_bonds, alternatives, cash], weights=[35, 15, 20, 10, 5, 1])
 
-  sample_scenario = Scenario("Sample", sample_portfolio)
-  sample_scenario.simulate_for(30)
-
+  sample_scenario = Scenario("Sample", sample_portfolio, 30, 15000.0, 2.0)
+  sample_scenario.simulate()
   print(str(sample_scenario))
+
+  mc = Monte_Carlo(sample_scenario)
+  mc.run(250)
+  print(str(mc))
+  mc.histogram()
 
